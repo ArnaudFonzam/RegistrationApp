2a4645aa8a13d4dcab1d0d51806de526bcfd66a0
davidofofou <guifofou9@gmail.com>
2022-12-10 12:35:42 +0100

    add error message when the username or password is not coorect

--- login.php
+++ login.php
@@ -1,3 +1,24 @@
+<?php
+require('config.php');
+session_start();
+if (isset($_POST['connect'])){
+  $username = stripslashes($_REQUEST['username']);
+  $username = mysqli_real_escape_string($conn, $username);
+  $password = stripslashes($_REQUEST['password']);
+  $password = mysqli_real_escape_string($conn, $password);
+  $query = "SELECT * FROM `user` WHERE username='$username' and password='".hash('sha256', $password)."'";
+  $result = mysqli_query($conn,$query) or die(mysql_error());
+  $rows = mysqli_num_rows($result);
+  $message = "";
+  if($rows==1){
+      $_SESSION['username'] = $username;
+      header('location: index.php');
+  }else{
+    $message = "Le nom d'utilisateur ou le mot de passe est incorrect";
+  }
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,25 +30,6 @@
     <script src="js/script.js"></script>
 </head>
 <body>
-<?php
-require('config.php');
-session_start();
-if (isset($_POST['connect'])){
-  $username = stripslashes($_REQUEST['username']);
-  $username = mysqli_real_escape_string($conn, $username);
-  $password = stripslashes($_REQUEST['password']);
-  $password = mysqli_real_escape_string($conn, $password);
-    $query = "SELECT * FROM `user` WHERE username='$username' and password='".hash('sha256', $password)."'";
-  $result = mysqli_query($conn,$query) or die(mysql_error());
-  $rows = mysqli_num_rows($result);
-  if($rows==1){
-      $_SESSION['username'] = $username;
-      header('location: index.php');
-  }else{
-    $message = "Le nom d'utilisateur ou le mot de passe est incorrect.";
-  }
-}
-?>
     <div class="box">
         <h1 class="box-title">Login</h1>
         <form action="" method="post">
@@ -35,9 +37,11 @@ if (isset($_POST['connect'])){
             <input type="password" class="box-input" name="password" placeholder="password" id="pwd">
             <input type="submit" class="box-button" name="connect" value="connexion">
         </form>
-        <?php if (! empty($message)) { ?>
-            <p class="errorMessage"><?php echo $message; ?></p>
-        <?php } ?>
+        <?php 
+          if(isset($message)){
+            echo "<p class='erreur'>".$message."</p>";
+          }
+        ?>
         <p class="box-register"><a href="register.php">register</a></p>
         <p class="box-register"><a href="updateMdp.php">Forget Password?</a></p>
         
